47398164946770f26691a6aed9e0c924e743c6a7
Shekhar <shnain@microsoft.com>
2021-03-21 11:44:36 +0000

    [FEAT] monoid interface exported

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { Sum } from "./monoid/sum.ts";
 export { Product } from "./monoid/product.ts";
 export { Any } from "./monoid/any.ts";
+export type { IMonoid } from "./monoid/monoid.interface.ts"
