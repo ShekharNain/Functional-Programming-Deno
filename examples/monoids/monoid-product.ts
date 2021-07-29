0dbec677dc4d7eed17b2b14b79d15a867f4c20ab
Shekhar <shnain@microsoft.com>
2021-07-29 14:00:13 +0000

    FIX: assertion fix

--- examples/monoids/monoid-product.ts
+++ examples/monoids/monoid-product.ts
@@ -4,5 +4,5 @@ Deno.test("Calculate five factorial", () => {
   const arr = [1,2,3,4,5];
   const fiveFactorial = { val: arr[0]*arr[1]*arr[2]*arr[3]*arr[4] } // Re-implement this using reduce & Product monoid
 
-  assertEquals(120, fiveFactorial.val, `5! is 120`);
+  assertEquals(fiveFactorial.val, 120, `5! is 120`);
 });
--- examples/monoids/monoid-sum.ts
+++ examples/monoids/monoid-sum.ts
@@ -4,5 +4,5 @@ Deno.test("Calculate sum of first five natural numbers", () => {
   const arr = [1,2,3,4,5];
   const sumOfNum = { val: arr[0]+arr[1]+arr[2]+arr[3]+arr[4] } // Re-implement this using reduce & Sum monoid
 
-  assertEquals(15, sumOfNum.val);
+  assertEquals(sumOfNum.val, 15);
 });
